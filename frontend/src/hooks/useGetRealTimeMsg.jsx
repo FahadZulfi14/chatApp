020fbec985d53ada3b2de4f686c21beac11739a2
FahadZulfi14 <fahadali02256@gmail.com>
2025-03-07 00:45:12 +0500

    reset url

--- frontend/src/hooks/useGetRealTimeMsg.jsx
+++ frontend/src/hooks/useGetRealTimeMsg.jsx
@@ -1,28 +1,3 @@
-// import React, { useEffect } from 'react';
-// import {useDispatch, useSelector} from 'react-redux'
-// import { setMessages } from '../redux/messageSlice';
-
-// const useGetRealTimeMsg = () => {
-//  const {socket} = useSelector(store => store.socket);
-//  const {messages} = useSelector(store => store.message);
-//  const dispatch = useDispatch()
-//  useEffect(()=>{
-// socket?.on('newMsg',(newMsg)=>{
-//     dispatch(setMessages([...messages, newMsg]));
-//     console.log('newMsg', newMsg)
-    
-// })
-// },[socket, setMessages])
-
-// }
-// export default useGetRealTimeMsg;
-
-
-
-
-
-
-
 import { useEffect } from "react";
 import {useSelector, useDispatch} from "react-redux";
 import { setMessages } from '../redux/messageSlice.jsx';
