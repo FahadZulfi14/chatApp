afc060a68d7ad3ac74c1c996e37c6fa5fdac014f
FahadZulfi14 <fahadali02256@gmail.com>
2025-03-09 04:13:05 +0500

    change config.js

--- frontend/src/assets/config.jsx
+++ frontend/src/assets/config.jsx
@@ -1,2 +1,5 @@
-export const BACKEND_URL = import.meta.env.VITE_BACKEND_URL
+// export const BACKEND_URL = import.meta.env.VITE_BACKEND_URL
+export const BACKEND_URL = `https://kind-manifestation-production.up.railway.app`
+
+
 
